586c0d6d89ee9fceae5214f546ce450719ad75d1
yurimm-song <136744719+yurimm-song@users.noreply.github.com>
2024-01-13 00:15:09 -0500

    JS-Home

--- src/main.js
+++ src/main.js
@@ -8,3 +8,10 @@ document.addEventListener("scroll", () => {
     header.classList.remove("header--dark");
   }
 });
+
+// Transparent on Home section when scrolling down the page
+const home = document.querySelector(".home__container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
