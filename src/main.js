d9e897595e164b360dd05d174b7571f69281c367
yurimm-song <136744719+yurimm-song@users.noreply.github.com>
2024-01-13 00:19:50 -0500

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -15,3 +15,13 @@ const homeHeight = home.offsetHeight;
 document.addEventListener("scroll", () => {
   home.style.opacity = 1 - window.scrollY / homeHeight;
 });
+
+// Transparent on Arrow up button when scrolling down the page
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+});
