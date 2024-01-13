ecf606f0233be9e0272aa2d62fd1c5ba7c002034
yurimm-song <136744719+yurimm-song@users.noreply.github.com>
2024-01-13 00:07:49 -0500

    JS-Header

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// Dark styling on header when scrolling down the page
+const header = document.querySelector(".header");
+const headerHeight = header.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > headerHeight) {
+    header.classList.add("header--dark");
+  } else {
+    header.classList.remove("header--dark");
+  }
+});
