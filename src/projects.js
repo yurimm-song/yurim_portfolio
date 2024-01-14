3f70edfa80fb7f79286baed6e192f7f0fc7c44cf
yurimm-song <136744719+yurimm-song@users.noreply.github.com>
2024-01-14 00:33:17 -0500

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,34 @@
-'use strict';
+"use strict";
+
+// Project filtering Logic
+const categories = document.querySelector(".portfolio__categories");
+const projects = document.querySelectorAll(".project");
+const projectsContainer = document.querySelector(".projects");
+categories.addEventListener("click", (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return;
+  }
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector(".category--selected");
+  active.classList.remove("category--selected");
+  target.classList.add("category--selected");
+}
+
+function filterProjects(filter) {
+  projects.forEach((project) => {
+    if (filter === "all" || filter === project.dataset.type) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+  projectsContainer.classList.add("anim-out");
+  setTimeout(() => {
+    projectsContainer.classList.remove("anim-out");
+  }, 250);
+}
