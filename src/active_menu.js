0d62a1e3a68bb127e39350dc222e3bf20338145e
yurimm-song <136744719+yurimm-song@users.noreply.github.com>
2024-01-15 00:21:30 -0500

    Observer-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -4,16 +4,20 @@ const sectionIds = [
   "#skills",
   "#portfolio",
   "#resume",
-  "contact",
+  "#contact",
 ];
-
 const sections = sectionIds.map((id) => document.querySelector(id));
 const navItems = sectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
-const visibleSections = sectionIds.map(() => false);
 
-const options = {};
+const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
+
+const options = {
+  rootMargin: "-20% 0px 0px 0px",
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -25,18 +29,24 @@ function observerCallback(entries) {
     selectLastOne =
       index === sectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.IntersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
   });
-  console.log(visibleSections);
-  console.log(selectLastOne);
 
   const navIndex = selectLastOne
     ? sectionIds.length - 1
     : findFirstIntersecting(visibleSections);
-  console.log(sectionIds[navIndex]);
+  selectNavItem(navIndex);
 }
 
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
 }
